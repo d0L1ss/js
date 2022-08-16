fc6355cffd6f2f4fdaadd450989a1cc168b35788
Rasul Tokaev <tokaevrasul12345@gmail.com>
2022-08-16 19:01:07 +0300

    js

---
+++ index.js
@@ -0,0 +1,32 @@
+// console.log("Hello world")
+
+// let, const
+
+// Типы данных js
+let num = 1 //number
+
+
+let str1 = "My name FooFi" //string
+let str2 = 'My name FooFi'
+let str3 =  `My name FooFi`
+console.log(str3);
+
+let bool1 = true //boolean
+let bool2 = false
+
+str3 = "My name LooLi"
+console.log(str3);
+
+//          0 1 2 3 4 5 6
+let nums = [1,2,3,4,5,-10,2]
+//             0       1        2      3
+let names = ["Adam", "Ali", "Timur", "Mo"]
+console.log(nums);
+console.log(names);
+
+let nameAge = ["Tobey", 47, "Andrew", 38, "Tom", 26, "Ned", 25, "Garry", 37]
+console.log("Имя:", nameAge[0], ",", "Возраст:", nameAge[1]);
+console.log("Имя:", nameAge[2], ",", "Возраст:", nameAge[3]);
+console.log("Имя:", nameAge[4], ",", "Возраст:", nameAge[5]);
+console.log("Имя:", nameAge[6], ",", "Возраст:", nameAge[7]);
+console.log("Имя:", nameAge[8], ",", "Возраст:", nameAge[9]);
