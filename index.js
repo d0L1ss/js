80d1c69e46b29454a52a8bd050471e23b59e53af
Rasul Tokaev <tokaevrasul12345@gmail.com>
2022-08-21 12:09:27 +0300

    js

--- index.js
+++ index.js
@@ -1,32 +1,70 @@
-// console.log("Hello world")
+// let str = "This is Macbook 13'3 M1"
+// let arr = [1, 2, 3, 4, 5,]
 
-// let, const
+// console.log("Длинна строки str", "=", str.length);
+// console.log("Длинна массива arr" + ":", arr.length);
 
-// Типы данных js
-let num = 1 //number
+// console.log(3<5);
+// console.log(3>5);
+// console.log(3<=3);
+// console.log(3>=2);
+// console.log("ab"==="ab");
+// console.log("ad"!=="ab");
+// console.log("ad"==="ad");
 
 
-let str1 = "My name FooFi" //string
-let str2 = 'My name FooFi'
-let str3 =  `My name FooFi`
-console.log(str3);
+// let age = 20
+// let myName = "Luda"
+// let horror = "Ужас"
+// let actionMovie = "Боевик"
+// let cartoon = "Мультик"
 
-let bool1 = true //boolean
-let bool2 = false
+// if (age >= 18) {
+//     console.log("Вам "+ age + " лет, поэтому смотреть можно " + horror + " жанр");
+// } else if (age > 12) {
+//     console.log("Вам " + age + " лет, поэтому смотреть можно " + actionMovie);
+// } else {
+//     console.log("Вам " + age + " лет, поэтому смотреть можно " + cartoon);
+// }
 
-str3 = "My name LooLi"
-console.log(str3);
 
-//          0 1 2 3 4 5 6
-let nums = [1,2,3,4,5,-10,2]
-//             0       1        2      3
-let names = ["Adam", "Ali", "Timur", "Mo"]
-console.log(nums);
-console.log(names);
+// console.log(s[0]);
+// console.log(s[1]);
+// console.log(s[2]);
+// console.log(s[3]);
+// console.log(s[4]);
+// console.log(s[5]);
+// console.log(s[6]);
+// console.log(s[7]);
+// let s = "Chechnya"
 
-let nameAge = ["Tobey", 47, "Andrew", 38, "Tom", 26, "Ned", 25, "Garry", 37]
-console.log("Имя:", nameAge[0], ",", "Возраст:", nameAge[1]);
-console.log("Имя:", nameAge[2], ",", "Возраст:", nameAge[3]);
-console.log("Имя:", nameAge[4], ",", "Возраст:", nameAge[5]);
-console.log("Имя:", nameAge[6], ",", "Возраст:", nameAge[7]);
-console.log("Имя:", nameAge[8], ",", "Возраст:", nameAge[9]);
+// for (let i = 0; i < s.length; i++) {
+//     console.log(s[i]);
+// }
+
+// for (let i = 22; i < 122; i+=2) {
+//     console.log(i);
+// }
+
+// for (let i = 2; i < 37; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// for (let i = 3; i < 37; i++) {
+//     if (i % 2 === 1) {
+//         console.log(i);
+//     }
+// }
+
+// for (let i = 0; i < 100; ++i) {
+//     if (i % 2 === 0) {
+//         console.log("chetnoe");
+//     }   else {
+//         console.log("nechetnoe");
+//     }
+// }
+
+// && - и
+// || - или
